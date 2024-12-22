4e44cb447efdf83fbcb8592d6b89efb5036419be
Seamoon Pandey <hackedasocial@gmail.com>
2024-12-23 01:19:37 +0545

    uypdate model

--- lib/model/event.dart
+++ lib/model/event.dart
@@ -5,8 +5,10 @@ class Event {
   final DateTime startTime;
   final DateTime endTime;
   final String title;
+  final String eventToken;
 
   Event({
+    required this.eventToken,
     required this.id,
     required this.lat,
     required this.lng,
@@ -18,11 +20,12 @@ class Event {
   factory Event.fromJson(Map<String, dynamic> json) {
     return Event(
       id: json['id'],
-      lat: json['lat'],
-      lng: json['lng'],
+      lat: json['latitude'],
+      lng: json['longitude'],
       startTime: DateTime.parse(json['start_time']),
       endTime: DateTime.parse(json['end_time']),
       title: json['title'],
+      eventToken: json['event_token'],
     );
   }
 
